ae9cdfd102f3e9a3f2d3c3f22796bf8eeef3c373
juliangnr90 <juliangnr90@gmail.com>
2019-04-24 11:03:15 -0300

    info-component structure (no font-family)

---
+++ src2/info.js
@@ -0,0 +1,26 @@
+const info = [
+    {
+        title: ["Nuestros", "Espacios"],
+        hasBorder: true,
+        description: 'En Jagger contamos con el equipo adecuado y áreas preparadas para cada tipo de entrenamiento, donde llevarás tus ejercicios y tu estaodo en forma a otro nivel'
+    },
+    {
+        title: ["¿Por qué es", "para todos?"],
+        hasBorder: true, 
+        description: "Las necesidades humanas difieren en grado no en tipo, por ello ajustamos la carga (peso) y la intensidad (repeticiones, timepos de descanso) pero no cambiamos el programa. <b>Trabajamos con una intensidad relativa a cada persona y de manera gradual.</b>"
+    },
+    {
+        title: ["¡no hay", "excusas!"],
+        hasBorder: false,
+        description: "En crossfit trabajamos con las clases grupales, un profesor o más, explicando la técnica de los ejercicios, mostrando variantes y corrigiendo. La buena noticia es que solamente necesitas de 1hr por día."
+    },
+    {
+        color: 'white',
+        backgroundImage: 'leon.jpg',
+        title: [""],
+        hasBorder: false,
+        description: [{text: "CrossFit...", uppercase: false}, "Es un programa de preparación física general amplio en <b>inclusivo</b>", "a través de movimientos dinámicos y <b>seguros</b>. Utilizamos ejercicios de peso corporal ejercicios con dominio de algún objeto externo. Que nos van a permitir mejorar una amplia gama de capacidades físicas."]
+    }
+];
+
+export default info;
--- src2/pages/Home.js
+++ src2/pages/Home.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import FooComponent from '../components/FooComponent';
+import InfoComponent from '../components/InfoComponent';
+import data from '../info';
 
 import './Home.scss';
 
-class Home extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div>
-                <h1 className="home-style">
-                    Home: <FooComponent/>
-                </h1>
-                <img src="static/images/leon.jpg"/>
-            </div>
-        )
-    }
-}
+const Home = () => (
+    <React.Fragment>
+        <InfoComponent {...data[0]} />
+        <InfoComponent {...data[1]} />
+        <InfoComponent {...data[2]} />
+        <InfoComponent {...data[3]} />
+    </React.Fragment>
+);
 
 export default Home;
