064fd4ad507141a5409720634b69d21497aa0cfa
Rulo180 <martinvalles@eventbrite.com>
2019-04-27 18:56:07 -0300

    Banner Component - Create Banner component with scss file. - Add Banner component to Home.

---
+++ src2/components/Banner.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './Banner.scss'
+
+
+const Banner = () => {
+  	return (
+		<section className="banner">
+			<div className="row justify-content-center">
+				<h3><b className="d-block d-sm-inline">¡Reserv&aacute; hoy!</b> Tu primera clase es <b>GRATIS</b></h3>
+			</div>
+		</section>
+	)
+}
+
+export default Banner
