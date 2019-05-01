026a79d990eefffa175054dcde7f042be1eab797
Rulo180 <martinvalles@eventbrite.com>
2019-05-01 11:51:54 -0300

    BoxAction Refactor

--- src2/components/BoxAction.js
+++ src2/components/BoxAction.js
@@ -22,20 +22,20 @@ class BoxAction extends React.Component {
         };
 
         return (
-            <div className="box-action">
-                <div className="box-action__image-contanier" style={imageContainerStyle}>
-                    <h2 className="text-center text-white">{this.name}</h2>
-                    <div className="text-center">
+            <section className="box-action" style={imageContainerStyle}>
+                <div className="container">
+                    <div className="box-action__titles-container">
+                        <h2 className="box-action__title">{this.name}</h2>
                         <button className="box-action__button">¡QUIERO EMPEZAR!</button>
                     </div>
+                    {/* <div className="box-action__link">
+                        <div className="box-action__button-wrapper text-center text-white">
+                            <div className="box-action__button-color" style={backgroundColorStyle}></div> 
+                            <span>VER BOX</span>
+                        </div>
+                    </div> */}
                 </div>
-                <div className="box-action__link">
-                    <div className="box-action__button-wrapper text-center text-white">
-                        <div className="box-action__button-color" style={backgroundColorStyle}></div> 
-                        <span>VER BOX</span>
-                    </div>
-                </div>
-            </div>
+            </section>
         )
     }
 }
