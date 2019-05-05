4e8601f61f1a32d4c33484e765ef97c0f2bebdae
juliangnr90 <rullencio180@gmail.com>
2019-05-04 21:45:30 -0300

    completed features

--- src2/components/FeatureComponent.js
+++ src2/components/FeatureComponent.js
@@ -13,11 +13,11 @@ class FeatureComponent extends React.Component {
 	getItem(items) {
 		return items.map((f, idx) => {
 			return (
-				<div className="features__item row" key={idx}>
-					<div className="features__icon col-3 text-right">
+				<div className="features__item" key={idx}>
+					<div className="features__icon p-0 d-inline">
 						<img src="static/images/miras.png" />
 					</div>
-					<div className="features__text col-9 align-self-center">{f}</div>
+					<p className="features__text align-self-center d-inline">{f}</p>
 				</div>
 			)
 		});
@@ -43,12 +43,12 @@ class FeatureComponent extends React.Component {
             <div className="features">
                 <div className="container">
 					<div className="row">
-						<div className="col-12 col-lg-8">
+						<div className="col-12 col-lg-6">
 							<h1 className="features__title">MEJORAS EN...</h1>
 							{featuresList}
 						</div>
-						<div className="col-12 col-lg-5 d-lg-none">
-							<img className="w-100" src="static/images/icon-circle.png" />
+						<div className="features__image col-12 col-lg-6">
+							<img src="static/images/icon-circle.png" />
 						</div>
 					</div>
                 </div>
