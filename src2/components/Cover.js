2d605083b08ee63adafdddf3a12c3d0bac5bfb5c
juliangnr90 <juliangnr90@gmail.com>
2019-05-09 11:41:58 -0300

    cover-complete

---
+++ src2/components/Cover.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './Cover.scss';
+
+class Cover extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="cover">
+                <div className="container">
+					<div className="row">
+						<div className="col-12">
+                            <h3>El mejor momento es AHORA</h3>
+                            <h3>El mejor lugar es ACA</h3>
+                            <h4 className="cover__subtitle">¿Comenzamos?</h4>
+                        </div>
+						<div className="col"></div>
+					</div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Cover;
