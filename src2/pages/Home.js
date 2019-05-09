075004fea0ebebc3ef82a1bb3bac035d32a61aa1
Rulo180 <Rullencio180@gmail.com>
2019-05-08 21:49:33 -0300

    Layout Component - Create Layout component with Navbar and Footer. - Replace both components with Layout on Home.

--- src2/pages/Home.js
+++ src2/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './Home.scss';
+import Layout from '../components/Layout';
 import Banner from '../components/Banner';
 import BoxSection from '../components/BoxSection';
 import FeatureComponent from '../components/FeatureComponent';
@@ -13,15 +14,15 @@ import data from '../info';
 
 const Home = () => (
     <Router>
-        <Route path="/" component={Navbar} />
-        <Route path="/" component={() => (<InfoComponent {...data[0]} />)} />
-        <Route path="/" component={BoxSection} />
-        <Route path="/" component={Banner}/>
-        <Route path="/" component={() => (<InfoComponent {...data[3]} />)} />
-        <Route path="/" component={() => (<InfoComponent {...data[1]} />)} />
-        <Route path="/" component={FeatureComponent} />
-        <Route path="/" component={() => (<InfoComponent {...data[2]} />)} />
-        <Route path="/" component={Footer} />
+        <Layout>
+            <Route path="/" component={() => (<InfoComponent {...data[0]} />)} />
+            <Route path="/" component={BoxSection} />
+            <Route path="/" component={Banner}/>
+            <Route path="/" component={() => (<InfoComponent {...data[3]} />)} />
+            <Route path="/" component={() => (<InfoComponent {...data[1]} />)} />
+            <Route path="/" component={FeatureComponent} />
+            <Route path="/" component={() => (<InfoComponent {...data[2]} />)} />
+        </Layout>
     </Router>
 );
 
