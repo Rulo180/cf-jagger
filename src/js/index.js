ff3afdbcc4f987da1b1c55422c52fb49398d0cd5
juliangnr90 <rullencio180@gmail.com>
2019-05-16 11:30:56 -0300

    changed folder names from src2 and dist2 to src and dist

--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-import _ from 'lodash';
-import loadAssets from './loadAssets.js';
-import carousel from './carousel.js';
-
-
-//We need to require html and image files to copy then on dist folder (using 'file-loader' with webpack)
-loadAssets();
