8de7022cf252a0c5b186b68c74b15653528d8d9e
juliangnr90 <juliangnr90@gmail.com>
2019-05-24 12:10:35 -0300

    coach section large and extra large

---
+++ src/components/Coach.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import './Coach.scss';
+
+class Coach extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { 
+            separator
+         } = this.props;
+        
+        const containerClasses = classNames(
+            'coach__description-container',
+            {'coach__description-container--separator': separator},
+        );
+
+        return (
+            <div className="coach">
+                <div className="container">
+                    <div className="row align-items-sm-center">
+                        <div className="col-12 col-lg-6">
+                            <div className="row">
+                                <div className="col-12">
+                                    <h1 className="coach__title">NUESTRO COACH</h1>    
+                                    <div className={containerClasses}>
+                                        <p className="coach__description-text">
+                                            LUCAS NESSIER, DE 27 A&nTILDE;OS, ES PROFESOR DE EDUCACI&Oacute;N F&Iacute;SICA Y ENTRENADOR DE CROSSFIT NIVEL 2 (DOS), APASIONADO POR LA ACTIVIDAD F&Iacute;SICA Y LA SALUD EX JUGADOR DE F&Uacute;TBOL. SUS INICIOS EN EL CROSSFIT FUERON A FINES DEL A&nTILDE;O 2013.
+                                        </p>
+                                        <p className="coach__description-text">
+                                            SU PACIENCIA Y DESEMPE&nTILDE;O HA MEJORADO LA CALIDAD DE VIDA DE MUCHO DE SUS ALUMNOS CONTAGIANDO EL ENTUSIASMO, PASI&Oacute;N POR EL DEPORTE Y BRINDANDO UN AMBIENTE DE CONTENCI&Oacute;N
+                                        </p> 
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-12 col-lg-6">
+                            <img className="coach__image" src="static/images/tigres.jpg" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Coach;
--- src/pages/BoxPage.js
+++ src/pages/BoxPage.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-import CoverBox from './CoverBox';
+import CoverBox from '../components/CoverBox';
+import Coach from '../components/Coach';
 
 export class BoxPage extends Component {
     render() {
         return (
             <React.Fragment>
                 <CoverBox />
+                <Coach separator />
             </React.Fragment>
         )
     }
