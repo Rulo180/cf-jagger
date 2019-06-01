e57c92152e66013b74463bbcfa6c2e642669ad08
Rulo180 <Rullencio180@gmail.com>
2019-05-31 21:58:31 -0300

    Refactor Socials - Socials now recieve an array with socials objects. - Add padding left for each social item instead of using cols. - Create constants file with the social info for both box.

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+export const GODOY_CRUZ_SOCIALS_INFO =  [
+    {name: 'facebook', link: 'https://www.facebook.com/crossfitjagergc/', iconClass: "fab fa-facebook-square"},
+    {name: 'instagram', link: 'https://www.instagram.com/crossfitjagergc/', iconClass: "fab fa-instagram"},
+];
+
+export const MAIPU_SOCIALS_INFO =  [
+    {name: 'facebook', link: 'https://www.facebook.com/crossfitjager/', iconClass: "fab fa-facebook-square"},
+    {name: 'instagram', link: 'https://www.instagram.com/crossfitjager/', iconClass: "fab fa-instagram"},
+    {name: 'whatsapp', link: 'tel:+5492614166859', iconClass: "fab fa-whatsapp"},
+];
