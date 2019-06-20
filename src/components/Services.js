008bb7e83e0cd2a5a8d1a8a1622e44435114ec47
Rulo180 <Rullencio180@gmail.com>
2019-06-20 18:19:01 -0300

    Services Mobile - Add adaptative design for mobile in Services. - Add react accessible accordion package. - Add Carousel component to MaipuPage.

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+	AccordionItemPanel,
+	AccordionItemButton,
+} from 'react-accessible-accordion';
+
 import './Services.scss';
 import Card from './Card';
 
@@ -13,7 +21,7 @@ const Services = () => {
 						<h2 className="services__title">En J&auml;ger Maip&uacute; te ofrecemos...</h2>
 					</div>
 				</div>
-				<div className="row justify-content-center">
+				<div className="row justify-content-center d-none d-md-flex">
 					<div className="col-10 col-md-6 col-lg-3 mb-5 mb-lg-0">
 						<Card 
 							title="Crossfit j&auml;ger" 
@@ -39,6 +47,92 @@ const Services = () => {
 						/>
 					</div>
 				</div>
+				<Accordion allowZeroExpanded className="d-md-none">
+					<AccordionItem className="services__accordion-item">
+						<AccordionItemHeading className="services__accordion-heading">
+							<AccordionItemButton className="services__accordion-item-button">
+								<div className="row no-gutters align-items-center h-100">
+									<div className="services__accordion-heading-icon">
+										<img src="/static/images/miras.png" alt="Mira" />
+									</div>
+									<h4 className="mb-0 text-uppercase">Crossfit j&auml;ger</h4>
+								</div>
+							</AccordionItemButton>
+						</AccordionItemHeading>
+						<AccordionItemPanel>
+							<div className="row no-gutters services__accordion-content">
+								<div className="col-9">
+									<p>
+										Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non id dolore dicta nihil iste omnis rem, ducimus pariatur eveniet, qui alias beatae impedit natus ipsam, unde veniam perferendis fuga ea illo deleniti tempore eius aspernatur voluptate. Voluptatum doloribus perspiciatis quo.
+									</p>
+								</div>
+							</div>
+						</AccordionItemPanel>
+					</AccordionItem>
+					<AccordionItem className="services__accordion-item">
+						<AccordionItemHeading className="services__accordion-heading">
+							<AccordionItemButton className="services__accordion-item-button">
+								<div className="row no-gutters align-items-center h-100">
+									<div className="services__accordion-heading-icon">
+										<img src="/static/images/miras.png" alt="Mira" />
+									</div>
+									<h4 className="mb-0 text-uppercase">j&auml;ger 45'</h4>
+								</div>
+							</AccordionItemButton>
+						</AccordionItemHeading>
+						<AccordionItemPanel>
+							<div className="row no-gutters services__accordion-content">
+								<div className="col-9">
+									<p>
+										Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non id dolore dicta nihil iste omnis rem, ducimus pariatur eveniet, qui alias beatae impedit natus ipsam, unde veniam perferendis fuga ea illo deleniti tempore eius aspernatur voluptate. Voluptatum doloribus perspiciatis quo.
+									</p>
+								</div>
+							</div>
+						</AccordionItemPanel>
+					</AccordionItem>
+					<AccordionItem className="services__accordion-item">
+						<AccordionItemHeading className="services__accordion-heading">
+							<AccordionItemButton className="services__accordion-item-button">
+								<div className="row no-gutters align-items-center h-100">
+									<div className="services__accordion-heading-icon">
+										<img src="/static/images/miras.png" alt="Mira" />
+									</div>
+									<h4 className="mb-0 text-uppercase">j&auml;ger kids</h4>
+								</div>
+							</AccordionItemButton>
+						</AccordionItemHeading>
+						<AccordionItemPanel>
+							<div className="row no-gutters services__accordion-content">
+								<div className="col-9">
+									<p>
+										Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non id dolore dicta nihil iste omnis rem, ducimus pariatur eveniet, qui alias beatae impedit natus ipsam, unde veniam perferendis fuga ea illo deleniti tempore eius aspernatur voluptate. Voluptatum doloribus perspiciatis quo.
+									</p>
+								</div>
+							</div>
+						</AccordionItemPanel>
+					</AccordionItem>
+					<AccordionItem className="services__accordion-item">
+						<AccordionItemHeading className="services__accordion-heading">
+							<AccordionItemButton className="services__accordion-item-button">
+								<div className="row no-gutters align-items-center h-100">
+									<div className="services__accordion-heading-icon">
+										<img src="/static/images/miras.png" alt="Mira" />
+									</div>
+									<h4 className="mb-0 text-uppercase">Levantamiento</h4>
+								</div>
+							</AccordionItemButton>
+						</AccordionItemHeading>
+						<AccordionItemPanel>
+							<div className="row no-gutters services__accordion-content">
+								<div className="col-9">
+									<p>
+										Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non id dolore dicta nihil iste omnis rem, ducimus pariatur eveniet, qui alias beatae impedit natus ipsam, unde veniam perferendis fuga ea illo deleniti tempore eius aspernatur voluptate. Voluptatum doloribus perspiciatis quo.
+									</p>
+								</div>
+							</div>
+						</AccordionItemPanel>
+					</AccordionItem>
+				</Accordion>
 			</div>
 		</section>
 	)
