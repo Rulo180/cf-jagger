6011e54c6720f76caf99397b76af9d494138cb51
Rulo180 <Rullencio180@gmail.com>
2019-06-22 15:09:51 -0300

    Add images to Carousel in both Pages.

--- src/constants.js
+++ src/constants.js
@@ -11,3 +11,5 @@ export const MAIPU_SOCIALS_INFO =  [
 
 export const MAIPU_APP_LINK = "https://jager.turnosweb.com/";
 export const GODOY_CRUZ_APP_LINK = "https://jager.turnosweb.com/";
+
+export const MAIPU_CAROUSEL_IMAGES = ['jager-maipu-01.jpg', 'jager-maipu-02.jpg', 'jager-maipu-03.jpg', 'jager-maipu-04.jpg', 'jager-maipu-05.jpg', 'jager-maipu-07.jpg', 'jager-maipu-08.jpg', 'jager-maipu-09.jpg', 'jager-maipu-10.jpg', 'jager-maipu-11.jpg', 'jager-maipu-12.jpg', 'jager-maipu-13.jpg', 'jager-maipu-14.jpg', 'jager-maipu-15.jpg', 'jager-maipu-16.jpg'];
--- src/pages/GodoyCruzPage.js
+++ src/pages/GodoyCruzPage.js
@@ -8,16 +8,18 @@ import InfoSection from '../components/InfoSection';
 import Socials from '../components/Socials';
 import {GODOY_CRUZ_SOCIALS_INFO, GODOY_CRUZ_APP_LINK} from '../constants';
 
+const IMAGES = ['tigres.jpg', 'gmap-maipu.png', 'tigres.jpg', 'gmap-maipu.png', 'tigres.jpg', 'gmap-maipu.png', 'tigres.jpg', 'gmap-maipu.png'];
+
 export class GodoyCruzPage extends Component {
     render() {
         return (
             <main>
-                <CoverBox actionLink={GODOY_CRUZ_APP_LINK} />
+                <CoverBox actionLink={GODOY_CRUZ_APP_LINK} box="godoyCruz" />
                 {/* <InfoSection title={"J&auml;ger Maip&aacute;"} description="Hola a todos" /> */}
                 <Banner />
                 <Coach separator />
                 <Socials socials={GODOY_CRUZ_SOCIALS_INFO}/>
-                <Carousel />
+                <Carousel images={IMAGES} initialIndex={0} />
             </main>
         )
     }
